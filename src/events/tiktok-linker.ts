ec59645901d45d950c64b9a39b4f46ad6e1301f5
NafDev <54936211+NafDev@users.noreply.github.com>
2022-04-23 02:29:37 +0100

    Change tiktok scraper to use Puppeteer

--- src/events/tiktok-linker.ts
+++ src/events/tiktok-linker.ts
@@ -1,5 +1,3 @@
-import axios from 'axios';
-import * as cheerio from 'cheerio';
 import { Client } from 'discord.js';
 import logger from '../helpers/logger';
 import BotEvent from './event-interface';
@@ -10,52 +8,42 @@ export const event: BotEvent = {
 };
 
 const tiktokRegexps = [
-  /https:\/\/vm.tiktok.com\/(?<videoid>[A-Za-z0-9]+)\//,
-  /(?:@[a-zA-z0-9]*|.*)(?:\/.*\/|trending.?shareId=)(?<videoid>[\d]*)/,
+  /https:\/\/vm\.tiktok\.com\/[A-Za-z0-9]+\//,
+  /https:\/\/www\.tiktok\.com\/@[A-Za-z0-9]+\/video\/[\d]+/,
 ];
 
 async function run(client: Client): Promise<void> {
   client.on('messageCreate', async (message) => {
     if (message.author.bot) return;
 
-    try {
-      for (const regexp of tiktokRegexps) {
-        const find = message.content.match(regexp);
-        if (find && find.groups.videoid) {
-          const videoId = find.groups.videoid;
-
-          const page = await axios.get(`https://proxitok.herokuapp.com/@placeholder/video/${videoId}`, {
-            headers: { accept: '*/*' },
-          });
-          const $ = cheerio.load(page.data);
-          const src = $('video source').attr('src');
-
-          const match = src.match(/(?<=\?url=).+/);
-
-          if (match) {
-            const url = decodeURIComponent(match[0]);
-            let shortUrl: string;
-
-            const config = {
-              headers: { Authorization: `Bearer JdFvkAeSGLXkCsn5bVQSbztJOiWLPdIDRJ5v9mruSacQbVXZqD1XBTgsjdA1` },
-            };
-
-            try {
-              const urlDayRes = await axios.post('https://www.urlday.com/api/v1/links', { url }, config);
-              urlDayRes;
-              shortUrl = urlDayRes.data.data.short_url;
-
-              await message.channel.send(shortUrl || url);
-            } catch (error) {
-              logger.warn(error);
-            }
-
-            return;
+    for (const regexp of tiktokRegexps) {
+      const find = message.content.match(regexp);
+      if (find) {
+        let videoSrc: string;
+        try {
+          videoSrc = await getTiktokVideoSrc(find[0]);
+          if (!videoSrc) {
+            throw new Error('videoSrc is null or undefined');
           }
+        } catch (error) {
+          logger.error('Failed to scrape TikTok video source');
+          return;
         }
+
+        await message.channel.send(videoSrc);
+        return;
       }
-    } catch (error) {
-      logger.error('Failed to link TikTok: ' + error);
     }
   });
+}
+
+import puppeteer from 'puppeteer';
+
+async function getTiktokVideoSrc(url: string): Promise<string> {
+  const browser = await puppeteer.launch({ headless: true });
+  const page = await browser.newPage();
+
+  await page.goto(url);
+
+  return page.evaluate('document.querySelector("video").getAttribute("src")').finally(() => browser.close());
 }
